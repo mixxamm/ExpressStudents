e5b56d2726fac11158eb4607eaaa7282aa7805ed
mixxamm <maximjanssensj5@gmail.com>
2020-01-20 11:14:59 +0100

    Rename product naar student
    Amai ik ben precies goe wakker haha

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var productRouter = require('./routes/product');
+var studentRouter = require('./routes/student');
 
 
 var app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/product', productRouter);
+app.use('/student', studentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/student.js
+++ routes/student.js
@@ -4,12 +4,14 @@ const MongoClient = require('mongodb').MongoClient
 
 MongoClient.connect('mongodb://localhost:27017', (err, database) => {
   if (err) return console.log(err)
-  db = database.db('naamVanDB')
+  db = database.db('exam')
 })
 
 
 router.get('/', function(req, res, next) {
-  res.render('lijst', {students: [{naam: "test"}]})
+  db.collection("students").find().toArray((err, response) => res.render('lijst', {students: response}))
+  //res.render('lijst', {students: [{naam: "test"}]})
 });
+
 
 module.exports = router;
